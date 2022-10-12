0d510b9d8ccde88e47109ea1583d6253bc9a0520
GitHub Action <action@github.com>
2022-10-12 09:15:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/xiaofeiyang110/intellijplatformplugindemo/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/xiaofeiyang110/intellijplatformplugindemo/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.xiaofeiyang110.intellijplatformplugindemo.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.xiaofeiyang110.intellijplatformplugindemo.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/xiaofeiyang110/intellijplatformplugindemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/xiaofeiyang110/intellijplatformplugindemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.xiaofeiyang110.intellijplatformplugindemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xiaofeiyang110.intellijplatformplugindemo.MyBundle
 
 class MyApplicationService {
 
